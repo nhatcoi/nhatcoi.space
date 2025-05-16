2111ed7dcbaf776020b9ebd3e01ccfdba386657a
Côi Nhật <coinhat@Cois-MacBook-Air.local>
2025-05-16 14:15:42 +0700

    add va alert

--- src/components/NavAlert.js
+++ src/components/NavAlert.js
@@ -8,6 +8,10 @@ export const NavAlert = () => {
         🚀 Seeking a FullStack Developer? Check out{' '}
           <Link to="/links">my resume</Link> and recent projects! 🚀
       </span>
+      <br />
+      <span>
+        💡 Do you want to build a website or work on a project? <Link to="/contact">Contact me</Link>.
+      </span>
     </section>
   );
 };
